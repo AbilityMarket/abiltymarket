0a3139c93a251b8baf9f392e84b48cbdcd62ed60
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2022-04-19 17:37:00 +0900

    초기세팅
    초기세팅

---
+++ abmarket/src/test/java/com/example/abmarket/AbmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
