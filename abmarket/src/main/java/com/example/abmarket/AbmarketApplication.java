7e968dd0b1887007a75dfd0423a440b7604278c2
dlxorb96 <xorb_1@naver.com>
2022-04-20 10:26:32 +0900

    초기세팅
    1. 파일이 없을 시 폴더 생성이 안됨.
    1.java파일 채워 넣었습니다.
    
    2. application에 초기 세팅

--- abmarket/src/main/java/com/example/abmarket/AbmarketApplication.java
+++ abmarket/src/main/java/com/example/abmarket/AbmarketApplication.java
@@ -1,13 +1,49 @@
 package com.example.abmarket;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+// filter적용시키기
+@ServletComponentScan(basePackages =  = {
+		"com.example.filter"
+})
+
+// 외부에서 설정값 끌어오기
+@PropertySource("classpath:global.properties")
+
+// 컨트롤러, 환경설정파일
+@ComponentScan(basePackages = {
+		"com.example.controller",
+		"com.example.config",
+		"com.example.restcontroller",
+		"com.example.service",
+		"com.example.jwt",
+		"com.example.schedule"
+})
+
+// 매퍼
+@MapperScan(basePackages = {
+	"com.example.mapper"
+})
+
+// 인테티(jpa)
+@EntityScan(basePackages = { "com.example.entity" })
+
+// 저장소(jpa)
+@EnableJpaRepositories(basePackages = { "com.example.repository" })
 
 @SpringBootApplication
 public class AbmarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbmarketApplication.class, args);
+		System.out.println("서버구동완료");
 	}
 
 }
