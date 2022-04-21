37b485e2ffc004cdd8b5e86ddaf62c392faca721
alsendrha <lwgus@naver.com>
2022-04-21 16:49:55 +0900

    asd

--- abmarket/src/main/java/com/example/entity/AbTipEntity.java
+++ abmarket/src/main/java/com/example/entity/AbTipEntity.java
@@ -53,5 +53,5 @@ public class AbTipEntity {
     // 팁이미지테이블
     @OneToMany(mappedBy = "abtip")
     private List<AbTipImageEntity> abimageList = new ArrayList<>();
-    asdasdasd
+    asdasdasdasdasd
 }
