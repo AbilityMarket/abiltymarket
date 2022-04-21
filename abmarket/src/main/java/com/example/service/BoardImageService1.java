aa0baab93e1ae7cd3f830231aeb81d1ad34e3736
alsendrha <lwgus@naver.com>
2022-04-21 16:42:55 +0900

    sdasdasdasd

---
+++ abmarket/src/main/java/com/example/service/BoardImageService1.java
@@ -0,0 +1,23 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.entity.BoardImageEntity;
+
+public interface BoardImageService1 {
+
+    // 이미지 등록하기
+    public long insertBoardImage(List<BoardImageEntity> list);
+
+    // 이미지 가져오기
+    public BoardImageEntity selectBoardImage(long bino);
+
+    // 이미지 수정
+    public int updateBoardImage(BoardImageEntity bimage);
+
+    // 이미지 삭제
+    public int deleteBoardImage(long bino);
+
+    // 이미지 가져오기
+    public List<Long> selectBoardImageOne(long bino);
+}
