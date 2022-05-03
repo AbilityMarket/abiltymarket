3949cdc13a2840865ceb7530ea3b997f7ce234ea
dlxorb96 <xorb_1@naver.com>
2022-05-03 14:14:17 +0900

    관리자 회원가입

--- abmarket/src/main/java/com/example/restcontroller/MemberRestController2.java
+++ abmarket/src/main/java/com/example/restcontroller/MemberRestController2.java
@@ -101,6 +101,46 @@ public class MemberRestController2 {
         return map;
     }
 
+    // 관리자 회원가입
+    @RequestMapping(value = "/joinAdmin", method = { RequestMethod.POST }, consumes = {
+            MediaType.ALL_VALUE }, produces = {
+                    MediaType.APPLICATION_JSON_VALUE })
+    public Map<String, Object> adminJoinPost(@ModelAttribute MemberEntity member,
+            @RequestParam(name = "file", required = false) MultipartFile file) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", 0);
+        try {
+            // 이미지 첨부
+            if (file != null) {
+                if (!file.isEmpty()) {
+                    member.setUimage(file.getBytes());
+                    member.setUimagename(file.getOriginalFilename());
+                    member.setUimagesize(file.getSize());
+                    member.setUimagetype(file.getContentType());
+                }
+
+            }
+
+            // 비밀번호 암호화하기
+            BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+            member.setUpw(bcpe.encode(member.getUpw()));
+            // 역할 세팅
+            member.setUrole("ADMIN");
+            int ret = memberService1.insertMember(member);
+            // 회원가입 성공 시 200 실패시 0
+            if (ret == 1) {
+                map.put("status", 200);
+            }
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+            // 에러발생시
+            map.put("status", -1);
+        }
+        return map;
+    }
+
     // 아이디 중복 확인
     @RequestMapping(value = "/check", method = { RequestMethod.GET }, consumes = { MediaType.ALL_VALUE }, produces = {
             MediaType.APPLICATION_JSON_VALUE })
