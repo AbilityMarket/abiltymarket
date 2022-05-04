3dc182f1beab62e9db66d487bea0a56173545c00
alsendrha <lwgus@naver.com>
2022-05-04 12:51:02 +0900

    1. Board 취합

--- abmarket/src/main/java/com/example/restcontroller/BoardRestController1.java
+++ abmarket/src/main/java/com/example/restcontroller/BoardRestController1.java
@@ -8,6 +8,8 @@ import com.example.entity.BoardEntity;
 import com.example.entity.MemberEntity;
 import com.example.jwt.JwtUtil;
 import com.example.service.BoardService1;
+import com.example.service.ChatService2;
+import com.example.service.CommService2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -29,6 +31,12 @@ public class BoardRestController1 {
 
     @Autowired
     JwtUtil jwtUtil;
+
+    @Autowired
+    ChatService2 cService2;
+
+    @Autowired
+    CommService2 commService2;
 
     // 게시글 작성
     // 127.0.0.1:9090/ROOT/api/board/insert
@@ -172,6 +180,50 @@ public class BoardRestController1 {
         } catch (Exception e) {
             e.printStackTrace();
             map.put("status", -1);
+        }
+        return map;
+    }
+
+    // 127.0.0.1:9090/ROOT/api/board/chatcount?bno=2
+    // 게시글 채팅 개수 조회
+    @RequestMapping(value = "/chatcount", method = { RequestMethod.GET }, consumes = {
+            MediaType.ALL_VALUE }, produces = {
+                    MediaType.APPLICATION_JSON_VALUE })
+    public Map<String, Object> chatcount(
+            @RequestParam(name = "bno") Long bno) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", 0);
+        try {
+            Long count = cService2.chatcount(bno);
+            if (count != null) {
+                map.put("status", 200);
+                map.put("count", count);
+            }
+        } catch (Exception e) {
+            map.put("status", -1);
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    // 127.0.0.1:9090/ROOT/api/board/commcount?bno=2
+    // 게시글 댓글 개수 조회
+    @RequestMapping(value = "/commcount", method = { RequestMethod.GET }, consumes = {
+            MediaType.ALL_VALUE }, produces = {
+                    MediaType.APPLICATION_JSON_VALUE })
+    public Map<String, Object> commcount(
+            @RequestParam(name = "bno") Long bno) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", 0);
+        try {
+            Long count = commService2.countComm(bno);
+            if (count != null) {
+                map.put("status", 200);
+                map.put("count", count);
+            }
+        } catch (Exception e) {
+            map.put("status", -1);
+            e.printStackTrace();
         }
         return map;
     }
