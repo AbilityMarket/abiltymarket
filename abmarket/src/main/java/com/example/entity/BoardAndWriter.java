08c9d504e2742ee694b23b9e047373095f2badab
dlxorb96 <xorb_1@naver.com>
2022-05-04 11:54:49 +0900

    2-3-1 메인 화면 뷰 엔티티만들기

---
+++ abmarket/src/main/java/com/example/entity/BoardAndWriter.java
@@ -0,0 +1,67 @@
+package com.example.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Immutable;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Immutable
+public class BoardAndWriter {
+    // 게시판 번호
+    @Id
+    Long bno;
+
+    // 제목
+    private String btitle;
+
+    // 내용
+    private String bcontent;
+
+    // 종류
+    private Long btag;
+
+    // 등록일자
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Date bregdate;
+
+    // 주소
+    private String baddress;
+
+    // 보드 이미지
+    private byte[] bimage;
+
+    private Long bimagesize;
+
+    private String bimagename;
+
+    private String bimagetype;
+
+    // 게시글 작성자 아이디
+    private String uid;
+
+    // 작성자 이미지
+    private byte[] uimage;
+
+    private Long uimagesize = 0L;
+
+    private String uimagetype;
+
+    private String uimagename;
+
+    // 작성자 신고당했니?
+    private Long ureported;
+
+    // 작성자 닉네임
+    private String unickname;
+
+}
