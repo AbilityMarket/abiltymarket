32815ee2e2a6795012b5e3a59c5f2b46d1dd13ba
dlxorb96 <xorb_1@naver.com>
2022-05-18 14:04:57 +0900

    2-8-4 마이페이지 리스트 ui 수정

---
+++ src/components/Mypage3.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="main">
+    <aside>
+      <v-expansion-panels multiple variant="accordion">
+        <v-expansion-panel class="elevation-0">
+          <v-expansion-panel-title>
+            <p style="margin-left: 8px">프로필</p>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ul class="profile">
+              <li @click="handleList('info')">정보</li>
+              <li>관심사 설정</li>
+              <li @click="handleList(1)">비밀번호 변경</li>
+              <li>알림 설정</li>
+            </ul>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            <p style="margin-left: 8px">마켓</p>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ul class="profile">
+              <li>찜 목록</li>
+              <li>내가 쓴 글</li>
+              <li>리뷰</li>
+              <li>내역</li>
+            </ul>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            <p style="margin-left: 8px">설정</p>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ul class="profile">
+              <li>차단 목록</li>
+              <li @click="handleList(2)">회원탈퇴</li>
+            </ul>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </aside>
+
+    <section>
+      <menu-1 v-if="state.components === 1"></menu-1>
+      <menu-2 v-if="state.components === 2"></menu-2>
+      <info v-if="state.components === 'info'"></info>
+    </section>
+  </div>
+</template>
+
+<script>
+import { reactive } from "@vue/reactivity";
+import menu1 from "./mypage/Menu1.vue";
+import menu2 from "./mypage/Menu2.vue";
+import info from "./mypage/Menu3.vue";
+import { onMounted } from "@vue/runtime-core";
+export default {
+  components: {
+    menu1,
+    menu2,
+    info,
+  },
+  setup() {
+    const state = reactive({
+        components: "",
+    })
+    onMounted(() => {});
+    const handleList = (no)=>{
+      state.components = no;
+      console.log(state.components)
+      console.log(no)
+      console.log(typeof(no))
+    }
+
+    return { 
+        state,
+        handleList,
+         };
+  },
+};
+</script>
+
+<style scoped src="../assets/css/mypage3.css">
+</style>
