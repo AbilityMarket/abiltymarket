dfba480dfbc3c55b701f2c8211c8c667575c2c62
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2022-05-18 22:33:58 +0900

    2-8-6 마이페이지 sectino ui 및 기능 추가
    주소 수정

--- src/components/mypage/Menu3.vue
+++ src/components/mypage/Menu3.vue
@@ -23,23 +23,56 @@
         </div>
       </div>
       <div class="right">
-        <label>이름</label>
-        
-        <label>닉네임</label>
-        <label>휴대전회</label>
-        <label>주소</label>
+        <div class="right_detail name">
+          <label>이름</label>
+          <div class="ddd">
+          <input class="right_input" type="text" 
+          :value="state.uname" readonly/>
+          </div>
+        </div>
+        <div class="right_detail">
+          <label>닉네임</label>
+          <div class="ddd">
+          <input class="right_input detail2" type="text"
+          :value="state.unickname" readonly />
+          <v-btn class="right_btn">수정</v-btn>
+          </div>
+        </div>
+        <div class="right_detail" >
+          <label>휴대전회</label>
+          <div class="ddd">
+          <input class="right_input detail2" type="text"
+           :value="state.uphone" readonly />
+           <v-btn class="right_btn">수정</v-btn>
+          </div>
+        </div>
+        <div class="right_detail">
+          <label>주소</label>
+          <div class="ddd">
+          <input class="right_input detail2" type="text"
+           :value="state.uaddress" readonly />
+           <v-btn  class="right_btn" @click="showApi">수정</v-btn>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import { reactive, ref } from "@vue/reactivity";
 export default {
   setup() {
     const imageFile = ref("null");
     const state = reactive({
       imgUrl: require("../../assets/images/디그다.png"),
+      uname: "미리",
+      uphone: "010-2553-4586",
+      unickname: "아이스아메리카",
+      uaddress: "아이스아메리카",
+      addr1:'',
+			addr2:''
     });
 
     const handleImageClick = () => {
@@ -56,11 +89,40 @@ export default {
         state.imgUrl = require("../../assets/images/디그다.png");
       }
     };
+
+    const showApi = async()=>{
+			new window.daum.Postcode({
+				 oncomplete: async(data) => {
+					 	let fullRoadAddr = data.roadAddress;
+						let extraRoadAddr = '';
+						if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+							extraRoadAddr += data.bname; 
+						}
+						if(data.buildingName !== '' && data.apartment === 'Y'){
+							extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+						}
+						if(extraRoadAddr !== ''){ 
+							extraRoadAddr = ' (' + extraRoadAddr + ')'; 
+						}
+						if(fullRoadAddr !== ''){ 
+							fullRoadAddr += extraRoadAddr; 
+						}
+						state.zip = data.zonecode; //5자리 새우편번호 사용 
+						state.uaddress = fullRoadAddr;
+                        const config = { headers: {Authorization : 'KakaoAK eddc9574385a3fb5f33707a8d3bfcb98'}};
+                        const url = 'https://dapi.kakao.com/v2/local/search/address.json?query='+state.uaddress;
+                        const response = await axios.get(url,config);
+                        console.log(response)
+                 }
+				 }).open();
+
+		}
     return {
       state,
       imageFile,
       handleImageClick,
       handleChangeImage,
+      showApi
     };
   },
 };
