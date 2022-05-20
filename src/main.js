853b1bc02f50afea99d8fb824e44e979aa3d8200
iswimwebb <iswimwebb@gmail.com>
2022-05-20 09:42:45 +0900

    3. sweetalert 에러 설정 추가

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import stores from './stores/index.js';
-
 import VueSweetalert2 from 'vue-sweetalert2';
 
 //router 설정
