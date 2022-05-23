ad609273e836c52678ded482c445af46f7c74663
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2022-05-23 22:57:59 +0900

    2-8-17 마이페이지 암호변경 프론트 유효성 검사

--- src/components/mypage/ChangePassword.vue
+++ src/components/mypage/ChangePassword.vue
@@ -1,103 +1,96 @@
 <template>
   <div class="main">
-		<h3>비밀번호 변경</h3>
+    <h3>비밀번호 변경</h3>
+    <div class="password">
+      <div>기존 비밀번호</div>
+      <input ref="pw1" v-model="state.pw1" type="password" />
+    </div>
 
-		
-			<Form @submit="onSubmit" :validation-schema="schema">
-				<div class="password">
-				<label for="email">기존 비밀번호</label>
-				<Field v-model="state.pw1" id="email_addr" name="email_addr" type="password" />
-				<ErrorMessage name="입력해주세요" />
-		</div>
-		<div class="new">
-			<label for="password">새로운 비밀번호</label>
-			<Field id="password" name="acc_pazzword" type="password"/>
-			<ErrorMessage name="acc_pazzword" />
+    <div class="new">
+      <div class="password">
+        <div>새로운 비밀번호</div>
+        <input ref="pw2" v-model="state.pw2" type="password" />
+      </div>
 
-			<label for="password">비밀번호 확인</label>
-			<Field id="password" name="acc_pazzword" type="password"/>
-			<ErrorMessage name="acc_pazzword" />
-		</div>
-
-				<button>Submit</button>
-			</Form>
-			<div>기존 비밀번호</div>
-			<input v-model="state.pw1" type="password">
-		</div>
-
-		<div class="new">
-			<div class="password">
-				<div>새로운 비밀번호</div>
-				<input v-model="state.pw2" type="password">
-			</div>
-
-			<div class="password">
-				<div>비밀번호 확인</div>
-				<input v-model="state.pw3" type="password">
-			</div>
-		</div>
-		<input type="button" value="비밀번호 변경" class="btn" @click="handleClick">
-	<!-- </div> -->
+      <div class="password">
+        <div>비밀번호 확인</div>
+        <input ref="pw3" v-model="state.pw3" type="password" />
+      </div>
+    </div>
+    <input type="button" value="비밀번호 변경" class="btn" @click="handleClick">
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import * as Yup from "yup";
-import { Field, Form, ErrorMessage } from "vee-validate";
-import { reactive } from '@vue/reactivity';
+import axios from "axios";
+import { reactive, ref } from "@vue/reactivity";
 export default {
-	components: {
-    Field,
-    Form,
-    ErrorMessage,
-  },
   setup() {
-
-	  const schema = Yup.object().shape({
-      email_addr: Yup.string().email().required().label("Email Address"),
-      acc_pazzword: Yup.string().min(5).required().label("Your Password"),
+    const pw1 = ref(null);
+    const pw2 = ref(null);
+    const pw3 = ref(null);
+    const state = reactive({
+      pw1: "",
+      pw2: "",
+      pw3: "",
     });
 
-    function onSubmit(values) {
-      alert(JSON.stringify(values, null, 2));
-    }
-		const state = reactive({
-			pw1:'',
-			pw2:'',
-			pw3:'',
-		})
+    const handleClick = async () => {
+      console.log("handleClick");
 
-		const handleClick = async()=>{
-			console.log("handleClick");
-			// 프론트 유효성 감사
+      if (state.pw1 === "") {
+        alert("기존 비밀번호를 입력해주세요");
+        pw1.value.focus();
+        return false;
+      }
+      if (state.pw2 === "") {
+        alert("새로운 비밀번호를 입력해주세요");
+        pw2.value.focus();
+        return false;
+      }
+      if (state.pw3 === "") {
+        alert("비밀번호 확인을 입력해주세요");
+        pw3.value.focus();
+        return false;
+      }
+      if (state.pw2 !== state.pw3) {
+        alert("비밀번호가 다릅니다.");
+        pw3.value.focus();
+        return false;
+      }
+      // 프론트 유효성 감사
 
-
-			const url = "/ROOT/api/member/changePw";
-			const headers = {
-				"content-type": "multipart/form-data" ,
-				"token" : sessionStorage.getItem("TOKEN")
-				};
-			const body = new FormData();
-			body.append("pw1", state.pw1);
-			body.append("pw2", state.pw2);
-			const response = await axios.post(url,body,{headers})
-			console.log(response);
-			if(response.data.status ===200){
-				alert("암호 변경 완료");
-				sessionStorage.setItem("TOKEN", response.data.token);
-				
-			}
-		}
+      const url = "/ROOT/api/member/changePw";
+      const headers = {
+        "content-type": "multipart/form-data",
+        token: sessionStorage.getItem("TOKEN"),
+      };
+      const body = new FormData();
+      body.append("pw1", state.pw1);
+      body.append("pw2", state.pw2);
+      const response = await axios.post(url, body, { headers });
+      console.log(response);
+      if (response.data.status === 200) {
+        alert("암호 변경 완료");
+        sessionStorage.setItem("TOKEN", response.data.token);
+      }
+      else if(response.data.status === 0){
+        alert("기존 비밀번호가 아닙니다.")
+      }
+      else{
+        alert("암호 변경을 실패했습니다.")
+      }
+    };
 
     return {
-			state,
-			handleClick,
-			schema,
-			onSubmit
-		};
+      pw1,
+      pw2,
+      pw3,
+      state,
+      handleClick,
+    };
   },
 };
 </script>
 
-<style scoped src="../../assets/css/changepassword2.css">
-</style>
+<style scoped src="../../assets/css/changepassword2.css"></style>
