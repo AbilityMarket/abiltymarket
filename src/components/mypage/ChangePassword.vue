06d9628760357f42489a0476111519284dc2e2bf
dlxorb96 <xorb_1@naver.com>
2022-05-23 17:50:54 +0900

    2-8-16 마이페이지 프론트 유효성 검사

--- src/components/mypage/ChangePassword.vue
+++ src/components/mypage/ChangePassword.vue
@@ -2,45 +2,98 @@
   <div class="main">
 		<h3>비밀번호 변경</h3>
 
-		<div class="password">
+		
+			<Form @submit="onSubmit" :validation-schema="schema">
+				<div class="password">
+				<label for="email">기존 비밀번호</label>
+				<Field v-model="state.pw1" id="email_addr" name="email_addr" type="password" />
+				<ErrorMessage name="입력해주세요" />
+		</div>
+		<div class="new">
+			<label for="password">새로운 비밀번호</label>
+			<Field id="password" name="acc_pazzword" type="password"/>
+			<ErrorMessage name="acc_pazzword" />
+
+			<label for="password">비밀번호 확인</label>
+			<Field id="password" name="acc_pazzword" type="password"/>
+			<ErrorMessage name="acc_pazzword" />
+		</div>
+
+				<button>Submit</button>
+			</Form>
 			<div>기존 비밀번호</div>
-			<input v-model="state.pw" type="password">
+			<input v-model="state.pw1" type="password">
 		</div>
 
 		<div class="new">
 			<div class="password">
 				<div>새로운 비밀번호</div>
-				<input v-model="state.pw1" type="password">
+				<input v-model="state.pw2" type="password">
 			</div>
 
 			<div class="password">
 				<div>비밀번호 확인</div>
-				<input v-model="state.pw2" type="password">
+				<input v-model="state.pw3" type="password">
 			</div>
 		</div>
 		<input type="button" value="비밀번호 변경" class="btn" @click="handleClick">
-	</div>
+	<!-- </div> -->
 </template>
 
 <script>
+import axios from 'axios';
+import * as Yup from "yup";
+import { Field, Form, ErrorMessage } from "vee-validate";
 import { reactive } from '@vue/reactivity';
 export default {
+	components: {
+    Field,
+    Form,
+    ErrorMessage,
+  },
   setup() {
+
+	  const schema = Yup.object().shape({
+      email_addr: Yup.string().email().required().label("Email Address"),
+      acc_pazzword: Yup.string().min(5).required().label("Your Password"),
+    });
+
+    function onSubmit(values) {
+      alert(JSON.stringify(values, null, 2));
+    }
 		const state = reactive({
-			pw:'',
 			pw1:'',
 			pw2:'',
+			pw3:'',
 		})
 
-		const handleClick = ()=>{
-			console.log("handleClick")
-			// 정규표현식 등장!!!!
-			// if(pw)
+		const handleClick = async()=>{
+			console.log("handleClick");
+			// 프론트 유효성 감사
+
+
+			const url = "/ROOT/api/member/changePw";
+			const headers = {
+				"content-type": "multipart/form-data" ,
+				"token" : sessionStorage.getItem("TOKEN")
+				};
+			const body = new FormData();
+			body.append("pw1", state.pw1);
+			body.append("pw2", state.pw2);
+			const response = await axios.post(url,body,{headers})
+			console.log(response);
+			if(response.data.status ===200){
+				alert("암호 변경 완료");
+				sessionStorage.setItem("TOKEN", response.data.token);
+				
+			}
 		}
 
     return {
 			state,
-			handleClick
+			handleClick,
+			schema,
+			onSubmit
 		};
   },
 };
