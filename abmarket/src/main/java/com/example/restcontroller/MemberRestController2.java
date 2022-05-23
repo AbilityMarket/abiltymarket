115a637d1bdf2e6823dce57e3e51ef4b93f5a389
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2022-05-23 22:59:50 +0900

    2-8-19 마이페이지 이미지 보드이미지, 로그인 한 유저 이미지, 보드 작성자 이미지

--- abmarket/src/main/java/com/example/restcontroller/MemberRestController2.java
+++ abmarket/src/main/java/com/example/restcontroller/MemberRestController2.java
@@ -1,5 +1,7 @@
 package com.example.restcontroller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,11 +13,15 @@ import com.example.service.MemberService1;
 import com.example.service.UserDetailsServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +44,9 @@ public class MemberRestController2 {
 
     @Autowired
     MemberRespository2 memberRespository2;
+
+    @Autowired
+    ResourceLoader resLoader;
 
     // 로그인
     @RequestMapping(value = "/login", method = { RequestMethod.POST }, consumes = { MediaType.ALL_VALUE }, produces = {
@@ -282,5 +291,69 @@ public class MemberRestController2 {
 
         }
         return map;
+    }
+
+    // 토큰으로 uid 뽑아내기(이미지 url 뽑아내기용)
+    @RequestMapping(value = "/tokenimage", method = { RequestMethod.GET }, consumes = {
+            MediaType.ALL_VALUE }, produces = {
+                    MediaType.APPLICATION_JSON_VALUE })
+    public Map<String, Object> tokenimage(
+            @RequestHeader(name = "token") String token) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", 0);
+        try {
+            String uid = jwtUtil.extractUsername(token);
+            if(uid !=null){
+                map.put("status", 200);
+                map.put("uid", uid);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("status", -1);
+
+        }
+        return map;
+    }
+    
+
+    // uid로 멤버 이미지 조회하기
+    // 127.0.0.1:9090/ROOT/api/member/image?uid=uid
+    @RequestMapping(value = "/image") // url 주소생성
+    public ResponseEntity<byte[]> imageGET(
+            @RequestParam(name = "uid") String uid)
+            throws IOException {
+
+        MemberEntity member = memberRespository2.findById(uid).orElse(null);
+
+        // 이미지가 있을때
+        if (member.getUimagesize() > 0) { // 첨부한 파일 존재
+            HttpHeaders headers = new HttpHeaders();
+
+            if (member.getUimagetype().equals("image/jpeg")) {
+                headers.setContentType(MediaType.IMAGE_JPEG);
+            } else if (member.getUimagetype().equals("image/png")) {
+                headers.setContentType(MediaType.IMAGE_PNG);
+            } else if (member.getUimagetype().equals("image/gif")) {
+                headers.setContentType(MediaType.IMAGE_GIF);
+            }
+
+            // 이미지 byte[], headers, HttpStatus.Ok
+            ResponseEntity<byte[]> response = new ResponseEntity<>(member.getUimage(),
+                    headers, HttpStatus.OK);
+            return response;
+        } else { // 이미지 없을때
+            InputStream is = resLoader
+                    .getResource("classpath:/static/images/default.jpg")
+                    .getInputStream();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            ResponseEntity<byte[]> response = new ResponseEntity<>(is.readAllBytes(),
+                    headers, HttpStatus.OK);
+            return response;
+        }
+
     }
 }
