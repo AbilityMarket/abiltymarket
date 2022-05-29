b84a719d8b6fae22487d4278460a1405387717a7
Hyun <lwgus@hanmail.net>
2022-05-30 08:32:03 +0900

    1. 문의게시판 추가

--- src/components/inquire/InquireList.vue
+++ src/components/inquire/InquireList.vue
@@ -2,10 +2,16 @@
     <div class="main">
         <!-- 문의내역이 있는경우 노출 -->
         <div v-if="state.empty5">
-            <div class="btn-box">
-                <router-link to="/InquireWrite"><v-btn class="btn">문의하기</v-btn></router-link>
+            <div class="banban">
+                <div class="btn-box">
+                    <router-link to="/InquireWrite"><v-btn class="btn">문의하기</v-btn></router-link>
+                </div>
+                <div class="search-box">
+                    <input v-model="state.title" class="search" @keyup.enter="handleData()" placeholder="검색">
+                    <v-btn class="btn1" @click="handleData()"> 검색 </v-btn>
+                </div>
             </div>
-           
+
             <v-table class="table" >
                 <thead>
                     <tr class="text-left">
@@ -13,6 +19,7 @@
                         <th class="text-left2"> 제목 </th>
                         <th class="text-left3"> 댓글유무 </th>
                         <th class="text-left4"> 작성일 </th>
+                        
                     </tr>
                 </thead>
                 <tbody>
@@ -20,24 +27,20 @@
                     <tr v-for="tmp in state.items" :key="tmp">
                         <td> {{ tmp.inqno }} </td>
                         <td class="link" @click="secretPage(tmp.secret)"> 
-                        <!-- 상세 페이지로 이동 -->
-                        <!-- <td class="link" @click="handleDetailPage(tmp.inqno)">  -->
+                        <!-- <td class="link" @click="handleDetailPage(tmp.inqno)"> 수정 전, 상세페이지로 이동 -->
                         {{ tmp.inqtitle }} <v-img src="../../assets/images/자물쇠.png" style="width:20px; height:20px; float:right; "/></td>
-                        <td v-if="tmp.inqtype == 0"> 완료 </td>
-                        <td v-if="tmp.inqtype == 1"> 미완료 </td>
-                        <td> {{tmp.inqregdate.split("T")[0]}} {{tmp.inqregdate.split("T")[1].split(":")[0]}}:{{tmp.inqregdate.split("T")[1].split(":")[1]}}
+                        <td v-if="tmp.inqtype == 0"> 완료 </td><td v-if="tmp.inqtype == 1"> 미완료 </td>   
+                        <td> {{tmp.inqregdate.split("T")[0]}} {{tmp.inqregdate.split("T")[1].split(":")[0]}}
+                             : {{tmp.inqregdate.split("T")[1].split(":")[1]}}
                         </td>
                     </tr>
                 </tbody>
             </v-table>
             <div class="text-center">
-               
-                    <!-- <div v-for="(tmp, idx) in state.total :key=tmp">
-                        
-                    </div> -->
-                
+
+
                 <v-pagination
-                    v-model="state.page" :length="6"
+                    v-model="state.page" :length="state.total"  
                     prev-icon="mdi-menu-left" next-icon="mdi-menu-right" @click="handleData()"
                 ></v-pagination>
             </div>
@@ -48,7 +51,7 @@
             <div class="in-content">
                 <p class="in-content-c">문의 내역이 없습니다.</p>
                 <p class="in-content-w"> 문의글을 작성해주세요.</p>
-                 <router-link to="/InquireWrite"><v-btn class="btn1">문의하기</v-btn></router-link>
+                 <router-link to="/InquireWrite"><v-btn class="btn2">문의하기</v-btn></router-link>
             </div>
         </div>
     </div>
@@ -67,13 +70,14 @@ export default {
             page : 1,
             title : '',
             select: 1,
-
+            total: 0,
         })
 
         // 상세페이지로 이동 
         // const handleDetailPage = (inqno) => {
         //     router.push({name:'SelectOne', query:{inqno:inqno}})
         // }
+
         //상세페이지로 이동 
         const secretPage = (inqno) => {
             router.push({name:'Secret', query:{inqno:inqno}})
@@ -89,7 +93,9 @@ export default {
             if(response.data.status === 200) {
                 console.log(response);
                 state.items = response.data.list;
-
+                state.title = response.data.title;
+                state.total = response.data.total;
+            
                 state.empty5 = true; 
             }
             else{
@@ -106,8 +112,7 @@ export default {
         return { 
             state, 
             handleData, 
-            // handleDetailPage,
-            secretPage
+            secretPage,            
         }
     }
 }
