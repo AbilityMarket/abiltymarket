466e70d56eae80363123613178e921480de48296
dlxorb96 <xorb_1@naver.com>
2022-05-30 12:32:40 +0900

    mqtt

---
+++ src/components/Mqtt.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <button @click="createConnection()">1. 서버연결</button>
+        <button @click="doSubscribe()">2. 토픽설정</button>
+        <button @click="doUnSubscribe()">3. 토픽해제</button>
+        <input type="text" v-model="state.message" />
+        <button @click="sendMessage()">4. 메세지보내기</button>
+    </div>
+</template>
+
+<script>
+import mqtt from 'mqtt';
+import { reactive } from '@vue/reactivity';
+import { onMounted } from '@vue/runtime-core';
+export default {
+    setup () {
+        const state = reactive({
+            client : '', //접속한 클라이언트 객체
+            host : '1.234.5.158',   //서버주소
+            port : 11884, //web용
+            options : {
+                clean : true, //세션초기화
+                reconnectPeriod : 20000, // 주기적인 접속 시간
+                // 고유값
+                clientId : 'd202_' + new Date().getTime(),
+                username : 'ds606', // 아이디
+                password : 'ds606', // 암호
+            },
+
+            topic : 'ds/class606/#',
+            qos : 0,  //quality of service 0부터 2까지의 숫자
+            // 상대방에게 정확하게 보내는 수치, 중요한 건 2번으로 보내야함. 대신 리소스를 많이 쓰게 됨.
+        });
+
+        
+
+        const createConnection= ()=>{
+            const url = `ws://${state.host}:${state.port}`;
+            try{
+                
+                state.client = mqtt.connect(url, state.options);
+                console.log(state.client);
+                state.client.on("connect", ()=>{
+                    console.log("connect success!")
+                })
+
+                state.client.on("error", ()=>{
+                    console.log("connect error!!");
+                })
+
+                state.client.on("message", (topic,message)=> {
+                    console.log(topic, JSON.parse(message));
+                })
+            }
+            catch(e){
+                console.log("mqtt error", e);
+            }
+        }
+
+        const doSubscribe = () => {
+            state.client.subscribe(state.topic, {qos:state.qos}, (error, res) => {
+                if(error) {
+                    console.log('subscribe topic error', error);
+                    return;
+                }
+                console.log('subscribe success', res);
+            });
+        };
+
+        const doUnSubscribe = () => {
+            state.client.unsubscribe(state.topic, error => {
+                if(error) {
+                    console.log('unsubscribe topic error', error);
+                    return;
+                }
+                console.log('unsubscribe success');
+            });            
+        };
+
+        
+        const sendMessage= ()=>{
+            // json object => string : JSON.stringify()
+            // string => json object : JSON.parse()
+
+            const payload = JSON.stringify({ userid: '', msg : state.message });
+
+            // 보낼 토픽, 보내는내용(문자), qos(0~2)
+            state.client.publish('ds/class606/d200' , payload, 0, error =>{
+                if(error){
+                    console.log("publish error", error);
+                }
+            })
+        }
+        
+        onMounted(()=>{
+            console.log(state.options.clientId);
+            createConnection()
+            doSubscribe()
+        });
+
+        return {
+            state,
+            createConnection,
+            doSubscribe,
+            doUnSubscribe,
+            sendMessage
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,33 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/components/Home';
 import Login from '@/components/Login';
 import Join from '@/components/Join';
+import JoinNext from '@/components/JoinNext';
 import Chat2 from '@/components/Chat2';
 import Address2 from '@/components/Address2';
 import Kakao2 from '@/components/Kakao2';
 import Trade2 from '@/components/Trade2';
+import Mypage3 from '@/components/Mypage3';
+import Detail from '@/components/Detail';
+import WriteReview from '@/components/mypage/WriteReview';
+import Logout from '@/components/Logout';
+
+import Mqtt from '@/components/Mqtt';
 
 const routes = [
     { path : '/', redirect:'/home'},
     { path : '/home', name:'Home', component:Home },
     { path : '/login', name:'Login', component:Login },
     { path : '/join', name:'Join', component:Join },
+    { path : '/joinnext', name:'JoinNext', component:JoinNext },
     { path : '/chat2', name:'Chat2', component:Chat2 },
     { path : '/address2', name:'Address2', component:Address2 },
     { path : '/kakao2', name:'Kakao2', component:Kakao2 },
     { path : '/trade2', name:'Trade2', component:Trade2 },
+    { path : '/mypage3', name:'Mypage3', component:Mypage3 },
+    { path : '/detail', name:'Detail', component:Detail },
+    { path : '/writeReview', name:'WriteReview', component:WriteReview },
+    { path : '/logout', name:'Logout', component:Logout },
+    { path : '/mqtt', name:'Mqtt', component:Mqtt },
 ];
 
 const router = createRouter(
