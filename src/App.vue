d2bba493e376ca4bda6b1a0cfc23f3ed8effbb46
aresgnos <95726560+aresgnos@users.noreply.github.com>
2022-06-02 14:44:18 +0900

    app.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-main>
       <v-container class="container" style="min-width: 1168px; width: 70%">
-        
+        <!-- 로그인 회원가입 버튼 -->
         <div class="d-flex flex-row-reverse">
           <div class="top">
             <router-link to="/login"
@@ -44,11 +44,8 @@
           </div>
         </div>
 
-        <!-- {{state.state.logged}}
-          {{state.logged}} -->
-
-        <header v-if="!state.clickLogged">
-          <!-- <header> -->
+        <!-- 로고, 메뉴들 -->
+        <div class="header">
           <div class="d-flex mb-6">
             <div class="logo">
               <a href="#"><v-img src="./assets/images/logo.jpg"></v-img></a>
@@ -67,7 +64,8 @@
               ></v-img>
             </button>
 
-            <div class="item" :class="'ml-auto'" style="margin-top: 10px">
+            <!-- 메뉴 오른쪽 부분 -->
+            <div class="item" :class="'ml-auto'">
               <ul class="menu">
                 <li>
                   <a href="#"
@@ -78,41 +76,63 @@
                   <a href="#">당신의&nbsp;능력</a>
                 </li>
                 <li><a href="#">고객센터</a></li>
-                <li>
-                  <v-row v-if="logged">
-                    <v-menu bottom min-width="400px" rounded offset-y>
-                      <template v-slot:activator="{ props }">
-                          <v-btn size="25" icon v-bind="props" @click="alertList">
-                              <v-avatar color="purple" size="25">
-                                <span class="text-h6">{{state.alertCnt}}</span>
-                              </v-avatar>
-                          </v-btn>
-                      </template>
-                      <v-card>
-                          <v-list>
-                              <v-list-item-group v-if="state.items">
-                                  <v-list-item v-for="tmp of state.items" :key="tmp">
-                                      <v-list-item-content v-text="tmp.almessage" style="cursor:pointer;" @click="clickOne(tmp.alno)"></v-list-item-content>
-                                  </v-list-item>
-                              </v-list-item-group>
-                          </v-list>
-                      </v-card>
-                    </v-menu>
-                  </v-row>알림
-                </li>
-                <li>
-                  <a href="#"><router-link to="chat2">채팅</router-link></a>
-                </li>
+
                 <li>
                   <a href="#"
                     ><router-link to="mypage3">마이페이지</router-link></a
                   >
                 </li>
+
+                <!-- 알림 -->
+                <li style="margin-top:5px;">
+                  <v-row v-if="logged">
+                    <v-menu bottom min-width="400px" rounded offset-y>
+                      <template v-slot:activator="{ props }">
+                        <v-btn size="25" icon v-bind="props" @click="alertList">
+                          <v-avatar
+                            color="#3476d8"
+                            size="23"
+                            style="margin-left: 30px"
+                          >
+                            <span class="text-h6">{{ state.alertCnt }}</span>
+                          </v-avatar>
+                        </v-btn>
+                      </template>
+                      <v-card>
+                        <v-list>
+                          <v-list-item-group v-if="state.items">
+                            <v-list-item v-for="tmp of state.items" :key="tmp">
+                              <v-list-item-content
+                                v-text="tmp.almessage"
+                                style="cursor: pointer"
+                                @click="clickOne(tmp.alno)"
+                              ></v-list-item-content>
+                            </v-list-item>
+                          </v-list-item-group>
+                        </v-list>
+                      </v-card>
+                    </v-menu> </v-row
+                  ><ion-icon
+                    name="notifications-outline"
+                    class="alert_icon"
+                  ></ion-icon>
+                </li>
+
+                <li style="margin-top:5px;">
+                  <a href="#"
+                    ><router-link to="chat2"
+                      ><ion-icon
+                        name="chatbubbles-outline"
+                        class="chat_icon"
+                      ></ion-icon></router-link
+                  ></a>
+                </li>
               </ul>
             </div>
           </div>
-        </header>
+        </div>
 
+        <!-- 각각 페이지 바뀌는 곳 -->
         <div class="main">
           <router-view></router-view>
         </div>
@@ -129,13 +149,13 @@
 </template>
 
 <script>
-import mqtt from 'precompiled-mqtt';
+import mqtt from "precompiled-mqtt";
 import { computed, onMounted, reactive, watch } from "vue";
 import Swal from "sweetalert2";
 import { useRouter } from "vue-router";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   setup() {
@@ -144,7 +164,6 @@ export default {
     const store = useStore();
 
     const state = reactive({
-      clickLogged: computed(() => store.getters.getClicklogged),
       client: "", //접속한 클라이언트 객체
       host: "1.234.5.158", //서버주소
       port: 11884, //web용
@@ -162,38 +181,38 @@ export default {
       // 상대방에게 정확하게 보내는 수치, 중요한 건 2번으로 보내야함. 대신 리소스를 많이 쓰게 됨.
 
       // 알림 badge 관련
-      token : sessionStorage.getItem("TOKEN"),
-      alertCnt : "",
+      token: sessionStorage.getItem("TOKEN"),
+      alertCnt: "",
     });
 
     // 알림 개수
-    const alertCnt = async() => {
+    const alertCnt = async () => {
       const url = `/ROOT/api/alert/alunreadcnt`;
       const headers = {
-        "Content-Type" : "application/json",
-        "token" : state.token
-      };        
-      const response = await axios.get(url, {headers});
+        "Content-Type": "application/json",
+        token: state.token,
+      };
+      const response = await axios.get(url, { headers });
       // console.log(response.data.result);
-      if(response.data.status ===200) {
-          // 알림 개수
-          state.alertCnt = response.data.result;
+      if (response.data.status === 200) {
+        // 알림 개수
+        state.alertCnt = response.data.result;
       }
     };
 
     // 배지 클릭 후 알림 목록
-    const alertList = async() => {
+    const alertList = async () => {
       // console.log("확인------");
       const url = `/ROOT/api/alert/alreadlist`;
       const headers = {
-        "Content-Type" : "application/json",
-        "token" : state.token                
+        "Content-Type": "application/json",
+        token: state.token,
       };
-      const response = await axios.get(url, {headers});
+      const response = await axios.get(url, { headers });
       // console.log(response);
       // console.log(response.data.list[0].almessage);
-      if(response.data.status === 200) {
-          state.items = response.data.list;
+      if (response.data.status === 200) {
+        state.items = response.data.list;
       }
     };
 
@@ -203,27 +222,27 @@ export default {
       // console.log(alno);
       const url = `/ROOT/api/alert/alselectone?alno=${alno}`;
       const headers = {
-        "Content-Type" : "application/json",
-        "token" : state.token
+        "Content-Type": "application/json",
+        token: state.token,
       };
-      const response = await axios.get(url, {headers});
+      const response = await axios.get(url, { headers });
       // console.log("확인222===");
       // console.log(response);
-      if(response.data.satus === 200) {
+      if (response.data.satus === 200) {
         router.push(response.data.result.alurl);
       }
     };
 
     // 채팅 연결
     const createConnection = () => {
-      console.log("App.vue 채팅연결")
+      console.log("App.vue 채팅연결");
       const url = `ws://${state.host}:${state.port}`;
       try {
         state.client = mqtt.connect(url, state.options);
         console.log(state.client);
         state.client.on("connect", () => {
           console.log("connect success!");
-          console.log("app.vue현재 구독중"+ state.topic);
+          console.log("app.vue현재 구독중" + state.topic);
         });
 
         state.client.on("error", () => {
@@ -284,26 +303,30 @@ export default {
     // stores의 getters 호출
     let logged = computed(() => store.getters.getLogged);
 
-    watch(() => (logged), (currentValue, oldValue) => {
-            console.log("11111111111111111111111111111111111111111111111111",currentValue);
-            console.log(oldValue);
-            if(sessionStorage.getItem("UID") ===null){
-              state.topic = "ds/abilitymarket/unlogged/#"
-            }
-            else{
-              state.topic ="ds/abilitymarket/" + sessionStorage.getItem("UID")+ '/#';
-                console.log("watch topic", state.topic);
-            }
-                
-                
-                // createConnection();
-            // }
-        },
-        {
-            immediate : true,
-            deep:true
-        }
+    watch(
+      () => logged,
+      (currentValue, oldValue) => {
+        console.log(
+          "11111111111111111111111111111111111111111111111111",
+          currentValue
         );
+        console.log(oldValue);
+        if (sessionStorage.getItem("UID") === null) {
+          state.topic = "ds/abilitymarket/unlogged/#";
+        } else {
+          state.topic =
+            "ds/abilitymarket/" + sessionStorage.getItem("UID") + "/#";
+          console.log("watch topic", state.topic);
+        }
+
+        // createConnection();
+        // }
+      },
+      {
+        immediate: true,
+        deep: true,
+      }
+    );
 
     const handleMenu = (menu) => {
       console.log(menu);
@@ -336,32 +359,12 @@ export default {
 
     // 생명주기 (F5를 눌러야 수행, 새로고침이 수행됨, 한번만 가능)
     onMounted(() => {
-      
-      // 로그인페이지나 회원가입 페이지일 경우 위에 헤더 없애기
-      if (
-        window.location.href.split("/")[4] === "login" ||
-        window.location.href.split("/")[4] === "join"
-      ) {
-        sessionStorage.setItem("CLICKLOGGED", true);
-        console.log("here");
-        store.commit("setClicklogged", true);
-      } 
-      // 나머지 페이지 헤더 나오기
-      else {
-        sessionStorage.setItem("CLICKLOGGED", false);
-        console.log("here2");
-        store.commit("setClicklogged", false);
-      }
-      console.log("route.name", window.location.href.split("/")[4]);
-
-      console.log(sessionStorage.getItem("TOKEN"));
-
       // 로그인이 되어있을경우
       if (sessionStorage.getItem("TOKEN") !== null) {
         let token = sessionStorage.getItem("TOKEN");
-        state.topic = "ds/abilitymarket/"+ sessionStorage.getItem("UID");
+        state.topic = "ds/abilitymarket/" + sessionStorage.getItem("UID");
         let eventSource = new EventSource(subscribeUrl + "?TOKEN=" + token);
-        
+
         eventSource.addEventListener("connect", function (event) {
           console.log(event.data);
         });
@@ -464,19 +467,21 @@ export default {
         );
 
         store.commit("setLogged", true);
-        state.topic = "ds/abilitymarket/" + sessionStorage.getItem("UID") +"/#";
-        console.log("state.topic 로그인 되어있을경우 sessionstoragfe UID를 통해 가져와서 세팅한"+state.topic)
+        state.topic =
+          "ds/abilitymarket/" + sessionStorage.getItem("UID") + "/#";
+        console.log(
+          "state.topic 로그인 되어있을경우 sessionstoragfe UID를 통해 가져와서 세팅한" +
+            state.topic
+        );
 
         // 알림 badge
         alertCnt();
-        
       } else {
-        state.topic = "ds/abilitymarket/unlogged/#"
+        state.topic = "ds/abilitymarket/unlogged/#";
         store.commit("setLogged", false);
       }
 
       // createConnection()
-
     });
 
     return {
@@ -521,109 +526,7 @@ export default {
   font-style: normal;
 }
 
-ul,
-li {
-  list-style: none;
-}
 
-a {
-  text-decoration: none;
-  color: #707070;
-  line-height: 100%;
-}
-
-button {
-  all: unset;
-}
-
-.logo {
-  width: 127px;
-}
-.container {
-  font-family: "GmarketSansMedium";
-}
-
-.top {
-  text-align: center;
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-
-.btn_login {
-  all: unset;
-  width: 125px;
-  height: 35px;
-  border: 1px solid #3476d8;
-  border-radius: 15px;
-  font-size: 0.9rem;
-  cursor: pointer;
-  margin-right: 10px;
-}
-
-.btn_logout {
-  all: unset;
-  width: 125px;
-  height: 35px;
-  border: 1px solid #3476d8;
-  border-radius: 15px;
-  font-size: 0.9rem;
-  cursor: pointer;
-  margin-right: 10px;
-}
-
-.btn_join {
-  all: unset;
-  width: 125px;
-  height: 35px;
-  background-color: #3476d8;
-  border: 1px solid #3476d8;
-  border-radius: 15px;
-  font-size: 0.9rem;
-  cursor: pointer;
-  color: #ffffff;
-}
-
-.btn_write {
-  all: unset;
-  width: 125px;
-  height: 35px;
-  background-color: #3476d8;
-  border: 1px solid #3476d8;
-  border-radius: 15px;
-  font-size: 0.9rem;
-  cursor: pointer;
-  color: #ffffff;
-}
-
-.menu > li {
-  display: inline-block;
-  margin-left: 30px;
-  font-size: 1rem;
-}
-
-.searchbox {
-  margin-left: 100px;
-}
-
-input {
-  border-bottom: 2px solid #3476d8;
-  border-left: none;
-  border-right: none;
-  border-top: none;
-  font-size: 16px;
-  font-family: "GmarketSansLight";
-}
-input:focus {
-  outline: none;
-}
-input::placeholder {
-  color: #6797dd;
-}
 </style>
 
-<style>
-.swal2-icon.swal2-info {
-  border-color: #3476d8 !important;
-  color: #3476d8 !important;
-}
-</style>
+<style scoped src="./assets/css/app.css"></style>
